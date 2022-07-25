539617800f2d4d32aa72b2400b9a10c9b6e4bff4
AlexeyScherbakov2021 <oppose2@yandex.ru>
2022-07-25 16:27:03 +0400

    Не работает обновление списка пользователей

--- Orders/Views/SettingWindow.xaml.cs
+++ Orders/Views/SettingWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Orders.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,5 +25,9 @@ namespace Orders.Views
             InitializeComponent();
         }
 
+        private void tab_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            (DataContext as RouteControlViewModel).RefreshCommand.Execute(this);
+        }
     }
 }
