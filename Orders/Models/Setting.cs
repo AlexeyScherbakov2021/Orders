e383ed9991846b8dd94e07b60a83eee3b4401ff4
AlexeyScherbakov2021 <oppose2@yandex.ru>
2025-05-05 08:15:03 +0400

    Добавлены настройки

---
+++ Orders/Models/Setting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders.Models
+{
+
+    [Table("Setting")]
+    public class Setting : IEntity
+    {
+        public int id { get; set; }
+        public string s_name { get; set; }
+        public string s_value { get; set; }
+        public string s_measure { get; set; }
+        public string s_descript { get; set;}
+    }
+}
